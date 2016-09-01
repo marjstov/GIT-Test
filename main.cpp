1c8583245feec08f39b795147382c5f6b41480f5
Martin Stovall <marjstov@ut.utm.edu>
2016-08-31 21:22:42 -0500

    Modified cpp with sum

--- main.cpp
+++ main.cpp
@@ -2,9 +2,20 @@
 
 using namespace std;
 
+int Sum(int z){
+  for(int i = 1; i <= z; i++)
+    {
+      cout << "Sum of 1 to " << i << " is " << i*(i+1)/2 << endl;
+    }
+}
 int main(){
 
+  int x;
+
   cout << "Hello World!" << endl;
+
+  cout << "What number do you want to pick to get the sum?";
+  cin >> x;
 
   return 0;
 }
